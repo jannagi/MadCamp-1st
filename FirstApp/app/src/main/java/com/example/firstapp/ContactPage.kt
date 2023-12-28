57a74f04c270f833df12baa538fa9bb9e86d8cf1
Jiyeon Na <skwldus@hanyang.ac.kr>
2023-12-28 18:37:28 +0900

    main page button ui changed, back button complete

--- FirstApp/app/src/main/java/com/example/firstapp/ContactPage.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/ContactPage.kt
@@ -1,11 +1,25 @@
 package com.example.firstapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ContactPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contact_page)
+
+        val backButton = findViewById<Button>(R.id.icon_back)
+        backButton.setOnClickListener {
+            navigateToPage(MainActivity::class.java)
+        }
+
+    }
+
+    private fun navigateToPage(cls: Class<*>) {
+        val intent = Intent(this, cls)
+        startActivity(intent)
+        finish()
     }
 }
