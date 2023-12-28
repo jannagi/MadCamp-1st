3179e7e0d3ad97ce8b6535b6220d32ba81caf096
Junhak Ha <hjh0868@kaist.ac.kr>
2023-12-28 18:40:04 +0900

    test

--- FirstApp/app/src/main/java/com/example/firstapp/ContactPage.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/ContactPage.kt
@@ -2,8 +2,9 @@ package com.example.firstapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-class ContactPage : AppCompatActivity() {
+class ContactPage : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contact_page)
--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -4,29 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_page)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val contactButton = findViewById<Button>(R.id.btnContactPage)
-        val galleryButton = findViewById<Button>(R.id.btnGalleryPage)
-        var thirdButton = findViewById<Button>(R.id.btnThirdPage)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.contactFragment,
+                R.id.galleryFragment,
+                R.id.thirdFragment
+            )
+        )
 
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         // 각 버튼에 대한 클릭 이벤트 처리
-        contactButton.setOnClickListener {
-            navigateToPage(ContactPage::class.java)
+        binding.btnContactPage.setOnClickListener {
+            navController.navigate(R.id.contactFragment)
         }
 
-        galleryButton.setOnClickListener {
-            navigateToPage(GalleryPage::class.java)
+        binding.buttonGallery.setOnClickListener {
+            navController.navigate(R.id.galleryFragment)
         }
 
-        thirdButton.setOnClickListener {
-            navigateToPage(ThirdPage::class.java)
+        binding.buttonThird.setOnClickListener {
+            navController.navigate(R.id.thirdFragment)
         }
     }
 
