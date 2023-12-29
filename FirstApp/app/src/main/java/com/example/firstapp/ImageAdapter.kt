e9a250f688ed5c68f8e0e3dd6687b3465e86c0c7
Junhak Ha <hjh0868@kaist.ac.kr>
2023-12-29 16:09:16 +0900

    Gallery Page

--- FirstApp/app/src/main/java/com/example/firstapp/ImageAdapter.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/ImageAdapter.kt
@@ -12,16 +12,24 @@ class ImageAdapter(private val context: Context) : BaseAdapter() {
         R.drawable.gallery_image_01,
         R.drawable.gallery_image_02,
         R.drawable.gallery_image_03,
-//        R.drawable.gallery_image_04,
-//        R.drawable.gallery_image_05,
-//        R.drawable.gallery_image_06,
-//        R.drawable.gallery_image_07,
-//        R.drawable.gallery_image_08,
-//        R.drawable.gallery_image_09,
-//        R.drawable.gallery_image_10,
+        R.drawable.gallery_image_04,
+        R.drawable.gallery_image_05,
+        R.drawable.gallery_image_06,
+        R.drawable.gallery_image_07,
+        R.drawable.gallery_image_08,
+        R.drawable.gallery_image_09,
+        R.drawable.gallery_image_10,
 
         // Add more dummy image resource IDs as needed
     )
+
+    private val targetImageSize: Int = calculateTargetImageSize(context)
+
+    private fun calculateTargetImageSize(context: Context): Int {
+        // Adjust this value based on the desired size of your images
+        val density = context.resources.displayMetrics.density
+        return (100 * density).toInt() // 100dp as an example, adjust as needed
+    }
 
     override fun getCount(): Int {
         return dummyImages.size
@@ -39,7 +47,9 @@ class ImageAdapter(private val context: Context) : BaseAdapter() {
         val imageView: ImageView
         if (convertView == null) {
             imageView = ImageView(context)
-            // Set layout parameters, scale type, etc.
+            imageView.layoutParams = ViewGroup.LayoutParams(targetImageSize, targetImageSize)
+            imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            imageView.setPadding(8, 8, 8, 8) // Optional: Add padding for better spacing
         } else {
             imageView = convertView as ImageView
         }
